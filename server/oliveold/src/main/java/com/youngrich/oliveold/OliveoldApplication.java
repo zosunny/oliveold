44b3a2cce66f51ec7809784c0056bcd1b8aef0ee
zosunny <zoheyy108@naver.com>
2024-11-13 02:40:18 +0900

    feat: project init

---
+++ server/oliveold/src/main/java/com/youngrich/oliveold/OliveoldApplication.java
@@ -0,0 +1,13 @@
+package com.youngrich.oliveold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveoldApplication.class, args);
+	}
+
+}
---
+++ server/oliveold/src/test/java/com/youngrich/oliveold/OliveoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngrich.oliveold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
